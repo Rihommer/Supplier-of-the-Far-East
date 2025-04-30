1e872216e837b19539a3f0a13f1f965eb05ee378
Vadim Yurasov <barracuda89.811@gmail.com>
2025-04-30 07:47:36 +0400

    add fonts

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,7 @@
 </script>
 
 <template>
-
+  <div style="font-family: 'Exo 2';">qwqwqw</div>
+  <div style="font-family: 'PT Sans';">qwqwqw</div>
+  <div style="font-family: 'PT Sans'; font-weight: 700;">qwqwqw</div>
 </template>
